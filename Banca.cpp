f998bc9bdd9c36a3c0edc613f0747f6819d72c1a
RaresIgescu <147654314+RaresIgescu@users.noreply.github.com>
2024-04-17 20:56:10 +0300

    Create Banca.cpp

---
+++ Banca.cpp
@@ -0,0 +1,28 @@
+//
+// Created by riges on 4/14/2024.
+//
+#include <iostream>
+#include <string>
+#include <utility>
+#include <vector>
+#include "Banca.h"
+
+Banca::Banca(string  nume, float tva,const vector <int>& rate): Nume(std::move(nume)), TVA(tva), NumarRate(rate) {}
+
+string Banca::GetNume() const{
+    return Nume;
+}
+
+float Banca::CalculPretFinal(float pret) const {
+    return pret + (pret * (TVA/100));
+}
+
+float Banca::GetRataLunara(float pret) const{
+    int NrRata = 0;
+    int Indice;
+    for(int i : NumarRate)
+        cout << ++NrRata << ". " << i << "\n";
+    cout << "Alegeti indecele numarului de rate." << "\n";
+    cin >> Indice;
+    return float(pret/NumarRate[Indice-1]);
+    }
