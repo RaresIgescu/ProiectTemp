eb34018552e60cfdda558f453648133bd44a7de8
RaresIgescu <147654314+RaresIgescu@users.noreply.github.com>
2024-04-18 14:18:35 +0300

    Update MasinaDiesel.cpp

--- src/MasinaDiesel.cpp
+++ src/MasinaDiesel.cpp
@@ -2,34 +2,17 @@
 // Created by riges on 4/15/2024.
 //
 #include <iostream>
-#include <cstring>
+#include <string>
+#include <utility>
 #include "MasinaDiesel.h"
 
 
 MasinaDiesel::MasinaDiesel(std::string marca, std::string model, float pret, int an, int rezervor, int poluare,
-                           std::string motor): Masina(marca, model, pret, an), CapacitateRezervor(rezervor), NivelPoluare(poluare), FirmaMotor(motor) {}
+                           std::string motor): Masina(std::move(marca), std::move(model), pret, an), CapacitateRezervor(rezervor), NivelPoluare(poluare), FirmaMotor(std::move(motor)) {}
 
 MasinaDiesel::~MasinaDiesel() =default;
 
-int MasinaDiesel::GetRezervor() const {
-    return CapacitateRezervor;
-}
-
-int MasinaDiesel::GetPoluare() const {
-    return NivelPoluare;
-}
-
-string MasinaDiesel::GetMotor() const {
-    return FirmaMotor;
-}
-
-MasinaDiesel & MasinaDiesel::operator=(const MasinaDiesel& diesel) {
-    Masina::operator=(diesel);
-    this->CapacitateRezervor = diesel.CapacitateRezervor;
-    this->NivelPoluare = diesel.NivelPoluare;
-    this->FirmaMotor = diesel.FirmaMotor;
-    return *this;
-}
+MasinaDiesel & MasinaDiesel::operator=(const MasinaDiesel& diesel) = default;
 
 bool MasinaDiesel::operator==(const MasinaDiesel &diesel) const{
     return Masina::operator==(diesel) and CapacitateRezervor == diesel.CapacitateRezervor and NivelPoluare == diesel.NivelPoluare;
