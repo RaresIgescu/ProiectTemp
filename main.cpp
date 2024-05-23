e6b77dcb45ca63bffc350b9035a3625e75582b4a
RaresIgescu <147654314+RaresIgescu@users.noreply.github.com>
2024-05-23 23:05:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,109 +1,41 @@
 #include <iostream>
 #include "DealerAuto.h"
 #include "MasinaElectrica.h"
+#include "Singleton.h"
 using namespace std;
 
 int main() {
+    Masina<string, double, int> masina;
     int continuare = 1;
     Dealer_Auto dealer;
+    Singleton* meniu = Singleton::PreiaInstanta();
+    meniu->setDealer(&dealer);
+    meniu->AdaugareOptiune(1, "Vizualizarea flotei de masini", &Dealer_Auto::VizualizareFlota);
+    meniu->AdaugareOptiune(2, "Verificarea stocului unei masini", &Dealer_Auto::VerificareStocMasina);
+    meniu->AdaugareOptiune(3, "Vinde o masina", &Dealer_Auto::Vinde);
+    meniu->AdaugareOptiune(4, "Cumpara o masina", &Dealer_Auto::Cumpara);
+    meniu->AdaugareOptiune(5, "Vinde mai multe masini", &Dealer_Auto::VindeMulte);
+    meniu->AdaugareOptiune(6, "Obtine pretul pe piata al unei masini",&Dealer_Auto::ObtinePret);
+    meniu->AdaugareOptiune(7, "Numarul de masini din fiecare categorie", &Dealer_Auto::NumaraObiecte);
+    meniu->AdaugareOptiune(8, "Verificare procent baterie", &Dealer_Auto::VerificareBaterieElectrice);
+    meniu->AdaugareOptiune(9, "Tipul fiecarei masini din flota",&Dealer_Auto::RTTI);
     while (continuare)
     {
         int InputContinuare;
-        cout << "Bine ati revenit in dealerul nostru autorizat, de ce ati fi interesat?" << "\n";
-        cout << "1. Vizualizare Flota Masini." << "\n";
-        cout << "2. Verificare Stoc." << "\n";
-        cout << "3. Vinde o masina." << "\n";
-        cout << "4. Cumpara o masina." << "\n";
-        cout << "5. Vinde mai multe masini. (Citire a n obiecte)" << "\n";
-        cout << "6. Obtine pretul pe piata al unei masini. (Upcasting)" << "\n";
-        cout << "7. Numarul total de masini din flota (Downcasting)" << "\n";
-        cout << "8. Verificare autonomie masina electrica (Ilustrare Exceptie)" << "\n";
-        cout << "9. Iesire." << "\n";
-        cin >> InputContinuare;
-        if(InputContinuare == 1) {
-            dealer.VizualizareFlota();
-            cout << "Continuati procesul?" << "\n";
-            cout << "1 - Da\n2 - Nu" << "\n";
-            cin >> InputContinuare;
-            if(InputContinuare == 1)
-                continuare = 1;
-            else
-                continuare = 0;
-        }
-        else if(InputContinuare == 2) {
-            dealer.VerificareStocMasina();
-            cout << "Continuati procesul?" << "\n";
-            cout << "1 - Da\n2 - Nu" << "\n";
-            cin >> InputContinuare;
-            if(InputContinuare == 1)
-                continuare = 1;
-            else
-                continuare = 0;
-        }
-        else if(InputContinuare == 3) {
-            dealer.Vinde();
-            cout << "Continuati procesul?" << "\n";
-            cout << "1 - Da\n2 - Nu" << "\n";
-            cin >> InputContinuare;
-            if(InputContinuare == 1)
-                continuare = 1;
-            else
-                continuare = 0;
-        }
-        else if (InputContinuare == 4) {
-            dealer.Cumpara();
-            cout << "Continuati procesul?" << "\n";
-            cout << "1 - Da\n2 - Nu" << "\n";
-            cin >> InputContinuare;
-            if(InputContinuare == 1)
-                continuare = 1;
-            else
-                continuare = 0;
-        }
-        else if(InputContinuare == 5) {
-            dealer.VindeMulte();
-            cout << "Continuati procesul?" << "\n";
-            cout << "1 - Da\n2 - Nu" << "\n";
-            cin >> InputContinuare;
-            if(InputContinuare == 1)
-                continuare = 1;
-            else
-                continuare = 0;
-        }
-        else if(InputContinuare == 6)
+        meniu->Afisare();
+        meniu->Alegere();
+        if(Singleton::iesire == 1)
         {
-              dealer.ObtinePret();
-            cout << "Continuati procesul?" << "\n";
-            cout << "1 - Da\n2 - Nu" << "\n";
-            cin >> InputContinuare;
-            if(InputContinuare == 1)
-                continuare = 1;
-            else
-                continuare = 0;
-        }
-        else if(InputContinuare == 7) {
-            dealer.NumaraObiecte();
-            cout << "Continuati procesul?" << "\n";
-            cout << "1 - Da\n2 - Nu" << "\n";
-            cin >> InputContinuare;
-            if(InputContinuare == 1)
-                continuare = 1;
-            else
-                continuare = 0;
-        }
-        else if(InputContinuare == 8) {
-            dealer.VerificareBaterieElectrice();
-            cout << "Continuati procesul?" << "\n";
-            cout << "1 - Da\n2 - Nu" << "\n";
-            cin >> InputContinuare;
-            if(InputContinuare == 1)
-                continuare = 1;
-            else
-                continuare = 0;
-        }
-        else if (InputContinuare == 9) {
             continuare = 0;
+            break;
         }
+        cout << "Continuati procesul?" << "\n";
+            cout << "1 - Da\n2 - Nu" << "\n";
+            cin >> InputContinuare;
+            if(InputContinuare == 1)
+                continuare = 1;
+            else
+                continuare = 0;
     }
     cout << "A fost o placere sa va avem langa noi, la revedere!" << "\n";
 }
