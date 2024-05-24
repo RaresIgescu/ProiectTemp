8e4301b7a11ba0515070b10404f4fd8474515a7d
RaresIgescu <147654314+RaresIgescu@users.noreply.github.com>
2024-05-24 13:17:22 +0300

    Delete src/sursa_MasinaD.cpp

--- src/sursa_MasinaD.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by riges on 5/20/2024.
-//
-#include <utility>
-
-#include "MasinaDiesel.cpp"
-
-template<> MasinaDiesel<string, double, int>::MasinaDiesel(const string& marca,const string& model, double pret, int an, int rezervor, int poluare,
-                                       const string& motor): Masina<string, double, int>(std::move(marca), std::move(model), pret, an), CapacitateRezervor(rezervor), NivelPoluare(poluare), FirmaMotor(std::move(motor)) {}
