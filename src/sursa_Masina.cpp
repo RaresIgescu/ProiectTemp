cb37faa54f0c508b118d7c3314220f20e66b9b7d
RaresIgescu <147654314+RaresIgescu@users.noreply.github.com>
2024-05-24 12:48:30 +0300

    Update sursa_Masina.cpp

--- src/sursa_Masina.cpp
+++ src/sursa_Masina.cpp
@@ -3,14 +3,4 @@
 //
 #include "Masina.cpp"
 
-template<> string Masina<string, double, int>::GetMarca() const {
-    return Marca;
-}
-
-template <> string Masina<string, double, int>::GetModel() const {
-    return Model;
-}
-
-template <> double Masina<string, double, int>::GetPret() const {
-    return Pret;
-}
+Masina<string, double, int> a;
